40f8ed21a4292b15e1ddb964d925d92ea6812772
i051108 <christophe.thiebaud@sap.com>
2016-12-24 15:48:00 +0100

    monsoon

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash 
 mvn clean deploy -DskipTests -DaltDeploymentRepository=nexus.wdf.sap.corp::default::http://nexus.wdf.sap.corp:8081/nexus/content/repositories/deploy.snapshots/
+mvn clean deploy -DskipTests -DaltDeploymentRepository=monsoon::default::http://mo-a1f3458fc.mo.sap.corp:8081/repository/maven-snapshots/
