f059b92c225ae09208f1ad5bc974e98c82f7e128
i051108 <christophe.thiebaud@sap.com>
2018-03-29 15:06:37 +0200

    added maven-compiler-plugin config for java 8

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
+#!/bin/bash 
+# -DskipTests 
 mvn deploy -DskipTests -DaltDeploymentRepository=nexus.wdf.sap.corp::default::http://nexussnap.wdf.sap.corp:8081/nexus/content/repositories/deploy.snapshots/
